cd4d81535c2c4996ae050f31c1caa152baeb87d3
Fernando Carvalho <nando_carvalhoo@hotmail.com>
2024-11-09 01:46:41 -0300

    revoke api key

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
                 Aqui está o local onde você pode me encontrar:
             </p>
             <iframe 
-                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAFCXp3GgBpF875Aof3Oe5vN3SpdM_HcFs&q=-15.760608,-47.884516" 
+                src="https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=-15.760608,-47.884516" 
                 width="600" 
                 height="450" 
                 style={{ border: 0 }} 
